c2d26871428b7ed31ab200d8c8fe62c16bb84cb9
mrslvs <sekerka.miroslav@outlook.com>
2021-02-21 12:56:34 +0100

    tested schwefel(testfn3) on population

--- schwefel.m
+++ schwefel.m
@@ -9,3 +9,8 @@ space = [space_down; space_up];
 pop_size = 100;
 
 population = genrpop(pop_size,space);
+fit_of_population = zeros(1, pop_size);
+
+for individ = 1:pop_size
+    fit_of_population(individ) = testfn3(population(individ));
+end
