d24a90f658996b7e3fcc0f132746f8f106236fd6
mrslvs <sekerka.miroslav@outlook.com>
2021-02-21 13:28:30 +0100

    tested for cycle + added comments

--- schwefel.m
+++ schwefel.m
@@ -2,20 +2,27 @@
 %isid92654
 %Schwefel
 
-space_down = ones(1,10) * -500;
-space_up = ones(1,10) * 500;
-space = [space_down; space_up];
+space_down = ones(1,10) * -500; %lowest value
+space_up = ones(1,10) * 500; %highest value
+space = [space_down; space_up]; %space used for generating population
+pop_size = 100; %population size(how many individuals)
+num_of_best_ones = [20, 15, 10, 5]; %how many individuals go to next generation (20x best, 15x second best, ...)
+num_of_cycles = 500;
 
-pop_size = 100;
 
-population = genrpop(pop_size,space);
-fit_of_population = zeros(1, pop_size);
+population = genrpop(pop_size,space); %generate population
 
-for individ = 1:pop_size
-    fit_of_population(individ) = testfn3(population(individ));
+fit_of_population = get_fit_of_population(pop_size, population);
+best_individuals = zeros(1,pop_size);
+
+for i = 1:num_of_cycles
+    fit_of_population = get_fit_of_population(pop_size, population);
+    best_individuals(i) = min(fit_of_population);
+    new_population = selbest(population, fit_of_population, num_of_best_ones);
 end
 
-function fit = get_fit_of_population(population, pop_size)
+
+function fit = get_fit_of_population(pop_size, population)
 %returns matrix of function values
     fit = zeros(1,pop_size);
     
