d9fe7748ce17fed362d38288c59be53766b96db4
mrslvs <sekerka.miroslav@outlook.com>
2021-02-21 12:55:03 +0100

    generated population

--- schwefel.m
+++ schwefel.m
@@ -1,3 +1,11 @@
 %uloha1
 %isid92654
 %Schwefel
+
+space_down = ones(1,10) * -500;
+space_up = ones(1,10) * 500;
+space = [space_down; space_up];
+
+pop_size = 100;
+
+population = genrpop(pop_size,space);
