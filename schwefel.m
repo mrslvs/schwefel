5a9b5da5eca150dc18fb6456445ee3287ed6587a
mrslvs <sekerka.miroslav@outlook.com>
2021-02-21 14:11:45 +0100

    added function get_selection

--- schwefel.m
+++ schwefel.m
@@ -6,7 +6,7 @@ space_down = ones(1,10) * -500; %lowest value
 space_up = ones(1,10) * 500; %highest value
 space = [space_down; space_up]; %space used for generating population
 pop_size = 100; %population size(how many individuals)
-num_of_best_ones = [20, 15, 10, 5]; %how many individuals go to next generation (20x best, 15x second best, ...)
+num_of_best_ones = [15, 10, 5]; %how many individuals go to next generation (3x best, 2x second best, ...)
 num_of_cycles = 500;
 
 %toggles
@@ -15,6 +15,7 @@ selbest_toggle = 1;
 seltourn_toggle = 0;
 selsus_toggle = 0;
 selrand_toggle = 0;
+selection = get_selection(selbest_toggle, seltourn_toggle, selsus_toggle, selrand_toggle);
 %mutation
 mutx_toggle = 1;
 muta_toggle = 0;
@@ -31,11 +32,32 @@ fit_of_population = get_fit_of_population(pop_size, population);
 best_individuals = zeros(1,pop_size);
 
 for i = 1:num_of_cycles
-    fit_of_population = get_fit_of_population(pop_size, population);
-    best_individuals(i) = min(fit_of_population);
+    fit_of_population = get_fit_of_population(pop_size, population); %fitness
+    best_individuals(i) = min(fit_of_population); %take best one for graph
     new_population = selbest(population, fit_of_population, num_of_best_ones);
+    
 end
 
+function sel = get_selection(best, tourn, sus, rand)
+%returns:
+%   1 for selbest    3 for selsus
+%   2 for seltourn   4 for selrand
+
+    %if 2 or more selection were toggled, display error message
+    if (best + tourn + sus + rand) >= 2
+        error('*** ERROR Please select only one selection ***');
+    end
+    
+    if best == 1
+        sel = 1;
+    elseif tourn == 1
+        sel = 2;
+    elseif sus == 1
+        sel = 3;
+    else
+        sel = 4;
+    end
+end 
 
 function fit = get_fit_of_population(pop_size, population)
 %returns matrix of function values
