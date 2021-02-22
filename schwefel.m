3b337d51568ee3b68b317ce94845e7a7a2a5ebfe
mrslvs <sekerka.miroslav@outlook.com>
2021-02-22 22:10:11 +0100

    first solution - mutation and crossing divided

--- schwefel.m
+++ schwefel.m
@@ -7,7 +7,7 @@ space_up = ones(1,10) * 500; %highest value
 space = [space_down; space_up]; %space used for generating population
 pop_size = 100; %population size(how many individuals)
 num_of_best_ones = [15, 10, 5]; %how many individuals go to next generation (3x best, 2x second best, ...)
-num_of_cycles = 500;
+num_of_cycles = 800;
 
 %toggles selection
 selbest_toggle = 0;
@@ -21,6 +21,7 @@ population = genrpop(pop_size,space); %generate population
 fit_of_population = get_fit_of_population(pop_size, population);
 best_individuals = zeros(1,pop_size);
 
+hold on
 for i = 1:num_of_cycles
     fit_of_population = get_fit_of_population(pop_size, population); %fitness
     best_individuals(i) = min(fit_of_population); %take best one for graph
@@ -37,11 +38,23 @@ for i = 1:num_of_cycles
             new_population = selrand(population, fit_of_population, sum(num_of_best_ones));
     end 
     
-    %mutation
-    temp_pop = mutx(population, 0.1, space);
+    %crossing
+    cross_pop = crossov(population, 1, 0);
     
+    %mutation
+    mut_pop = mutx(population, 0.5, space);
+    
+    %add to pop_size
+    diff = (pop_size-(sum(num_of_best_ones))) /2;
+    
+    %take from crossed and mutated
+    temp_pop1 = selrand(cross_pop, fit_of_population, diff);
+    temp_pop2 = selrand(mut_pop, fit_of_population, diff);
+    
+    population = [new_population; temp_pop1; temp_pop2];
 end
 
+plot(best_individuals);
 
 function sel = get_selection(best, tourn, sus, rand)
 %returns:
